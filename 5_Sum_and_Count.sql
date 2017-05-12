240b6404ef19bddaef7eada82eb6f472d7faf9e3
wangyiqun527 <yiqunwang.lisa@gmail.com>
2017-05-11 22:47:36 -0400

    completed

--- 5_Sum_and_Count.sql
+++ 5_Sum_and_Count.sql
@@ -9,7 +9,8 @@ SELECT DISTINCT continent FROM world
 SELECT sum(gdp) FROM world WHERE continent='Africa'
 
 --4. How many countries have an area of at least 1000000
-SELECT count(name) FROM world WHERE area>= 1000000
+SELECT COUNT(name) FROM world WHERE area>= 1000000
+--Note can use COUNT(DISTINCT id)
 
 --5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
 SELECT sum(population) FROM world
