01db8e062ee82856c1bcedfdefe89a23fe0c2fab
wangyiqun527 <yiqunwang.lisa@gmail.com>
2020-06-16 11:19:58 -0400

    Update 1_Select_basics.sql

--- 1_Select_basics.sql
+++ 1_Select_basics.sql
@@ -1,6 +1,6 @@
 --1. Modify it to show the population of Germany
 SELECT population FROM world
-  WHERE name = 'Germany'
+  WHERE name = 'Germany';
   
 --2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 SELECT name, population FROM world
@@ -11,5 +11,5 @@ SELECT name, population FROM world
 --shows countries with an area of 250,000-300,000 sq. km. Modify it to show the
 -- country and the area for countries with an area between 200,000 and 250,000.
 SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
+  WHERE area BETWEEN 200000 AND 250000;
   
